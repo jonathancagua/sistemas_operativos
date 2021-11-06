5d7202a1b90f342194a5dcdd0fe0b3a783e29392
Jonathan Cagua <jonathan.cagua@gmail.com>
2021-11-06 11:30:31 -0500

    FIX:  alinear

--- fork-class2.c
+++ fork-class2.c
@@ -38,31 +38,31 @@ int main(void)
 	switch(pid = fork())
 	{
 		case -1:
-            perror("fork"); /* something went wrong */
-            exit(1);
+                  perror("fork"); /* something went wrong */
+                  exit(1);
 		break;
 
 		/* parent exits */
 		case 0:
-            printf(" CHILD: This is the child process!\n");
-            printf(" CHILD: My PID is %d\n", getpid());
-            printf(" CHILD: My parent's PID is %d\n", getppid());
-            printf(" CHILD: Llamo a exec...\n ");
-            char* const args[] = {"/bin/ls","-lah",NULL};
-            execv("/bin/ls",args);
-            printf(" CHILD: I'm outta here!\n");
-            exit(rv);
+                  printf(" CHILD: This is the child process!\n");
+                  printf(" CHILD: My PID is %d\n", getpid());
+                  printf(" CHILD: My parent's PID is %d\n", getppid());
+                  printf(" CHILD: Llamo a exec...\n ");
+                  char* const args[] = {"/bin/ls","-lah",NULL};
+                  execv("/bin/ls",args);
+                  printf(" CHILD: I'm outta here!\n");
+                  exit(rv);
 		break;
 
 
 		default:
-            printf("PARENT: This is the parent process!\n");
-            printf("PARENT: My PID is %d\n", getpid());
-            printf("PARENT: My childs PID is %d\n", pid);
-            wait(&sarasa);
-            printf("PARENT: My child's exit status is: %d\n", WEXITSTATUS(sarasa));
-            printf("PARENT: I'm outta here!\n");
-        break;
+                  printf("PARENT: This is the parent process!\n");
+                  printf("PARENT: My PID is %d\n", getpid());
+                  printf("PARENT: My childs PID is %d\n", pid);
+                  wait(&sarasa);
+                  printf("PARENT: My child's exit status is: %d\n", WEXITSTATUS(sarasa));
+                  printf("PARENT: I'm outta here!\n");
+            break;
 	}
 }
 
