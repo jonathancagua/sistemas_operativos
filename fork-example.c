f4e0d494c98bf5c56d8b145eedc4ba2fe105cb50
Jonathan Cagua <jonathan.cagua@gmail.com>
2021-11-06 10:19:02 -0500

    ADD: ejemplo de proceso padre hijo y zombie

--- fork-example.c
+++ fork-example.c
@@ -13,10 +13,39 @@
  *      - retorna 0 en el proceso hijo
  *      - Si existe un error retorna -1, eso significa 
  *          que el hijo no fue creado
+ * - comandos:
+ *   kill -9 PID-proceso --> kill -9 27374
+ *   ps -lf para ver los procesos
  * @version 0.1
  * @date 2021-11-06
  * 
  * @copyright Copyright (c) 2021
  * 
  */
+#include <unistd.h>
+#include <stdio.h>
+int variable = 8;
+int main(void){
+    pid_t pidP;
+    printf("--> proceso inicia con PID = %d \n\r", getpid());
+    pidP = fork();
+    printf("--> proceso ejecutando PID = %d pidP= %d \n\r", getpid(),pidP);
 
+    if(pidP > 0){
+        variable = 9;
+        printf("proceso padre \n\r");
+    }
+    else if(pidP == 0){
+        variable = 11;
+        printf("proceso hijo \n\r");
+    }
+    else{
+        printf("ERROR el hijo no fue creado\n\r");
+    }
+    while(1){
+        sleep(2);
+        printf("proceso ejecutando PID = %d variable = %d \n\r", getpid(),variable);
+    }
+
+    return 0;
+}
